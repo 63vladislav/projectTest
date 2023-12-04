90fe3936ccb64445210d6f356ab2cf32a2f604f0
63vladislav <vladushka6321@icloud.com>
2023-12-04 18:11:29 +0400

    Initial Commit

---
+++ projectTest/projectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectTestApp.swift
+//  projectTest
+//
+//  Created by 111 on 04.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct projectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
